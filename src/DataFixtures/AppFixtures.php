75a62bd2eb7a2202875afe12cef0f62428ad26ab
Co-7 <colin.v42@protonmail.com>
2023-02-14 10:37:45 +0100

    Addition of different packages for the connection to the database and creation of the entity User with the implementation of fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+
+class AppFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordHasherInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user1 = new User();
+        $user1->setEmail('user1@example.com');
+        $user1->setPassword($this->passwordEncoder->hashPassword($user1, 'password1'));
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail('user2@example.com');
+        $user2->setPassword($this->passwordEncoder->hashPassword($user2, 'password2'));
+        $manager->persist($user2);
+
+        $manager->flush();
+    }
+}
