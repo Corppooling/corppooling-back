4e900c2051515ba09076c55015da1106e12d01b0
Mathis972 <mathis.marieclaire@ynov.com>
2023-03-30 12:49:01 +0200

    feat added twig back

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
