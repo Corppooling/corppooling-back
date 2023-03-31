20a6f3437ff80fa5dd4bacb956e5a17da2b5bbe9
Mathis <eyzex@hotmail.fr>
2023-03-31 10:56:50 +0200

    feat(entity): added group for department

--- src/Entity/Department.php
+++ src/Entity/Department.php
@@ -7,6 +7,7 @@ use App\Repository\DepartmentRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 
 #[ORM\Entity(repositoryClass: DepartmentRepository::class)]
@@ -16,9 +17,13 @@ class Department
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['show_trip', 'list_trip'])]
+
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['show_trip', 'list_trip'])]
+
     private $name;
 
     #[ORM\Column(type: 'datetime_immutable')]
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,6 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use stdClass;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
