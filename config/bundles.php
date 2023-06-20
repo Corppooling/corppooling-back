ad3efe91c59f2d46c9c5106215f361b88e6ff595
Mathis <eyzex@hotmail.fr>
2023-06-20 14:34:05 +0200

    feat(admin): added admin login

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
