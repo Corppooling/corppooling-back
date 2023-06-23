f2cb8f1b76e38016a9f15f77c6b437e9d6c54e5b
Mathis <eyzex@hotmail.fr>
2023-06-23 10:45:57 +0200

    feat(tests): added tests for login / access

--- tests/UserControllerTest.php
+++ tests/UserControllerTest.php
@@ -7,7 +7,41 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testDocAccess(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/docs');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('title', 'API Platform');
+    }
+
+    public function testAdminRedirect(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/admin');
+
+        $this->assertResponseRedirects();
+        $this->assertSelectorTextContains('title', 'login');
+    }
+
+    public function testAdminAccess(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $adminUser = $userRepository->findOneByEmail('admin@corppooling.com');
+        $client->loginUser($adminUser);
+
+        $client->request('GET', '/admin');
+        $response = $client->getResponse();
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testLogin(): void
     {
         $client = static::createClient();
         $userRepository = static::getContainer()->get(UserRepository::class);
@@ -17,8 +51,31 @@ class UserControllerTest extends WebTestCase
 
         // simulate $testUser being logged in
         $client->loginUser($testUser);
-        $crawler = $client->request('GET', '/api/user/me');
 
+        // check reponse
+        $client->request('GET', '/api/user/me');
+        $response = $client->getResponse();
         $this->assertResponseIsSuccessful();
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertTrue($responseData['email'] === $testUser->getEmail());
     }
+
+    // public function testRegister(): void
+    // {
+    //     $client = static::createClient();
+    //     $userRepository = static::getContainer()->get(UserRepository::class);
+
+    //     // retrieve the test user
+    //     $testUser = $userRepository->findOneByEmail('user1@example.com');
+
+    //     // simulate $testUser being logged in
+    //     $client->loginUser($testUser);
+
+    //     // check reponse
+    //     $client->request('GET', '/api/user/me');
+    //     $response = $client->getResponse();
+    //     $this->assertResponseIsSuccessful();
+    //     $responseData = json_decode($response->getContent(), true);
+    //     $this->assertTrue($responseData['email'] === $testUser->getEmail());
+    // }
 }
