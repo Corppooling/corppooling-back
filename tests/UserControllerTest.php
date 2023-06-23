e077ad9a81f80176e10ad770b926592c7f2066be
Mathis <eyzex@hotmail.fr>
2023-06-23 11:19:33 +0200

    feat: added company test

--- tests/UserControllerTest.php
+++ tests/UserControllerTest.php
@@ -59,10 +59,37 @@ class UserControllerTest extends WebTestCase
         $this->assertTrue($responseData['email'] === $testUser->getEmail());
     }
 
+    public function testCompanyInTrips(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test user
+        $testUser = $userRepository->findOneByEmail('user2@example.com');
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+        $company = $testUser->getCompany();
+        $companyId = $company->getId();
+        // check reponse
+        $client->request('GET', "/api/trips?company.id=$companyId");
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $responseData = json_decode($response->getContent(), true);
+        if (count($responseData['hydra:member'])) {
+            foreach ($responseData['hydra:member'] as $value) {
+                $this->assertTrue($value['company']['id'] === $companyId);
+            }
+        } else {
+            $this->assertTrue($responseData['hydra:totalItems'] === 0);
+        }
+    }
+
     // public function testRegister(): void
     // {
     //     $client = static::createClient();
     //     $userRepository = static::getContainer()->get(UserRepository::class);
+    //     $client->request('POST', '/api/register');
+
 
     //     // retrieve the test user
     //     $testUser = $userRepository->findOneByEmail('user1@example.com');
